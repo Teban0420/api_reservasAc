69fde432f1efde70b17f1bf162ed8496b2fabf8e
esteb <giraldoesteban044@gmail.com>
2024-03-25 11:21:03 -0500

    cancelar booking

--- src/api/Bookings.js
+++ src/api/Bookings.js
@@ -6,4 +6,13 @@ export const Bookings = axios.create({
         apikey: 'EnbX12j02DDHFrAoqjaq3FIkmTGncrrk',
         Accept: 'application/json'
     } 
-});  
+}); 
+
+export const CancelBooking = axios.create({
+    baseURL: 'https://api-gateway.champ.aero/booking',
+    headers: {
+        "apikey": 'EnbX12j02DDHFrAoqjaq3FIkmTGncrrk',
+        "Accept": 'application/json',
+        "If-Match": 85854555
+    }
+})
--- src/components/reservas/Booking.jsx
+++ src/components/reservas/Booking.jsx
@@ -1,14 +1,62 @@
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Button, Popconfirm } from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
 import queryString from 'query-string';
+import { CancelBooking } from '../../api/Bookings';
+
 
 export const Booking = () => {
 
     const location = useLocation();
+    const navigate = useNavigate();
     const { r = ''} = queryString.parse( location.search);
 
-    const bookingObj = JSON.parse(r);   
+    useEffect( () => {
+
+        if(!r){
+            navigate('/formulario');
+            return
+        }
+    }, []);
+    
+    const bookingObj = JSON.parse(r); 
+    const { businessId } = bookingObj;
+
+      
+    const CancelarBooking = async (id) => {
+        
+        const peticion = await CancelBooking.post(`v2/bookings/${id}/cancellation-requests`);
+
+        if(peticion.status === 204){
+            navigate('/formulario');
+        }
+    }
 
     return(
-        <h1>Booking </h1>
+        <>
+            <h1>Booking </h1>
+            <Popconfirm
+                title="Delete the booking"
+                description="Are you sure to delete this booking?"
+                onConfirm={() => CancelarBooking(businessId)}
+                icon={
+                <QuestionCircleOutlined
+                    style={{
+                    color: 'red',
+                    }}
+                />
+                }
+            >
+             <Button 
+                danger
+                style={{marginLeft: '40px'}}
+            >
+                Delete
+            </Button>
+
+            </Popconfirm>
+        </>
+
     )
 }
